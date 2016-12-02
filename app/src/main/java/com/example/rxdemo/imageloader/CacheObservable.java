55ad807fe7fdea2d77a2b3092b0dceca3a665442
huangyanzhen <huangyanzhen.sh@superjia.com>
2016-12-03 00:21:19 +0800

    create an abstact observable

---
+++ app/src/main/java/com/example/rxdemo/imageloader/CacheObservable.java
@@ -0,0 +1,26 @@
+package com.example.rxdemo.imageloader;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+
+/**
+ * Created by huangyanzhen on 2016/12/3.
+ */
+
+public abstract class CacheObservable {
+
+    public Observable<Image> getImage(final String url) {
+        return Observable.create(new ObservableOnSubscribe<Image>() {
+            @Override
+            public void subscribe(ObservableEmitter<Image> e) throws Exception {
+                Image image = getDataFromCache(url);
+                e.onNext(image);
+                e.onComplete();
+            }
+        });
+    }
+
+    public abstract Image getDataFromCache(String url);
+    public abstract void putDataIntoCache(Image image);
+}
