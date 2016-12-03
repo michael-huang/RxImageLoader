d04f25b0789a08c6a97aef02290d6785746d82c8
Michael Huang <hyc1124@gmail.com>
2016-12-03 15:39:25 +0800

    implement MemoryCacheObservable class

--- app/src/main/java/com/example/rxdemo/imageloader/MemoryCacheObservable.java
+++ app/src/main/java/com/example/rxdemo/imageloader/MemoryCacheObservable.java
@@ -1,12 +1,30 @@
 package com.example.rxdemo.imageloader;
 
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
 /**
  * Created by huangyanzhen on 2016/12/3.
  */
 
 public class MemoryCacheObservable extends CacheObservable {
+
+    int maxMemory = (int)(Runtime.getRuntime().maxMemory() / 1024); // KB
+    int cacheSize = maxMemory /8;
+
+    LruCache<String, Bitmap> mLruCache = new LruCache<String, Bitmap>(cacheSize) {
+        @Override
+        protected int sizeOf(String key, Bitmap value) {
+            return value.getRowBytes() * value.getHeight() / 1024;
+        }
+    };
+
     @Override
     public Image getDataFromCache(String url) {
+        Bitmap bitmap = mLruCache.get(url);
+        if (bitmap != null) {
+            return new Image(url, bitmap);
+        }
         return null;
     }
 
